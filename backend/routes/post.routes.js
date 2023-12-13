3b5e21d84e68283d748ee07bdb96a8aae8eaa495
Nino GUEGUEN <ninogueguen@gmail.com>
2023-12-13 11:50:21 +0100

    Lancement serveur, test des routes GET,POST,PUT,DELETE

---
+++ backend/routes/post.routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => res.json({ message: "Voici les données" }));
+
+router.post("/", (req, res) => {
+  res.json({ message: req.body.message });
+});
+
+router.put("/:id", (req, res) => {
+  res.json({ messageId: "Modification du post " + req.params.id });
+});
+
+router.delete("/:id", (req, res) => {
+  res.json({ messageId: "Suppression du post " + req.params.id });
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Middleware qui permet de traiter les données de la request
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/post", require("./routes/post.routes"));
+
+// Lancer le serveur
+app.listen(port, () => console.log("Le serveur a démarré au port : " + port));
